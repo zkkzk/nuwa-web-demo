5d64c487518a2420f5d5104c9408650edcd2faa4
zhangke <10066400+zhang-ke001@user.noreply.gitee.com>
2024-05-09 12:41:01 +0800

    feat:character selected publisled worldbook

--- NuwaCreation/src/app/ui/character/Plot_WorldBook.tsx
+++ NuwaCreation/src/app/ui/character/Plot_WorldBook.tsx
@@ -1,14 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "@/navigation";
 import { usePostCharaFun, getWorldBookList } from "@/app/lib/utils";
 import { useTranslations } from "next-intl";
-import { Button, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react";
+import { Button, CircularProgress, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react";
 import { TypeChara, TypeWorldBook } from "@/app/lib/definitions";
 import { useCharaListItem, useCharaListItemDispatch } from "@/app/contexts/CharasContextProvider";
 import WorldBookItem from "../worldbook/WorldBookItem";
 import UnLinkIcon from "@/app/icons/UnLinkIcon";
 import { LinkIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { getIsLogin } from "@/app/nuwa-login-ui/utils/base.api";
+import { getWorldBookAll } from "@/app/lib/worldbook.api";
 
 function Plot_WorldBook() {
   const router = useRouter();
@@ -18,8 +20,44 @@ function Plot_WorldBook() {
 
   const charaList = getWorldBookList();
   const [myWorldBooks , setMyWorldBooks] = useState(charaList || [] as Array<TypeWorldBook>);
-  const isLogin = false;
 
+  const [isInit, setIsInit] = useState(false);
+  const [firstInit, setFirstInit] = useState(true);
+  const isLogin = getIsLogin();
+  const getWorldBookAllApi = getWorldBookAll();
+  const [isLoading, setIsLoading] = useState(false);
+  const [ worldBookPublishList, setWorldBookPublishList ] = useState<{
+    world: {
+      data: TypeWorldBook,
+      uid: string,
+    }
+  }[]>([]);
+
+  useEffect(() => {
+    if (isLogin) {
+      !isInit && setIsInit(true)
+    } else {
+      setFirstInit(false)
+    }
+  }, [])
+
+  useEffect(() => {
+
+    if (isInit && isLogin) {
+      init();
+    }
+    
+  }, [isInit])
+
+  const init = async () => {
+    const res = await getWorldBookAllApi.send();
+    if (res && res.code === 0) {
+      setWorldBookPublishList(res.data);
+    }
+    
+    setIsInit(false);
+    setFirstInit(false)
+  }
 
   const charaListItem = useCharaListItem();
   const charaListItemDispatch = useCharaListItemDispatch();
@@ -101,26 +139,77 @@ function Plot_WorldBook() {
                 >{t('WorldBook.addnewbook')}</Button>
               </ModalHeader>
               <ModalBody>
-                <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-10 py-10 px-7 overflow-visible h-auto min-h-[400px]">
-                  {myWorldBooks && myWorldBooks.map((worldBookItem, index) => (
-                    <div
-                      key={`${worldBookItem.uid}+${index}`}
-                      onClick={() => {
-                        const {updateChara} = usePostCharaFun(charaListItem.chara, worldBookItem.worldBook);
-                        setCharaListItem(updateChara);
-                        onClose();
-                      }}
-                      className="cursor-pointer relative w-auto h-[340px]"
-                    >
-                      <WorldBookItem worldBookItem={worldBookItem.worldBook} />
-                    </div>
-                  ))}
-                  {!myWorldBooks || myWorldBooks.length === 0 && (
-                    <div className="w-full h-full flex justify-center items-center  md:col-span-4 sm:col-span-4 col-span-1">
-                      {t("WorldBook.worldbooknull")}
-                    </div>
+                <>
+                  {(myWorldBooks.length !== 0) ? (
+                    <>
+                      <div className="text-black text-3xl font-semibold">{t("WorldBook.drafts")}</div>
+                      <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-10 py-10 px-7 overflow-visible h-auto min-h-[400px]">
+                        {myWorldBooks && myWorldBooks.map((worldBookItem, index) => (
+                          <div
+                            key={`${worldBookItem.uid}+${index}`}
+                            onClick={() => {
+                              const {updateChara} = usePostCharaFun(charaListItem.chara, worldBookItem.worldBook);
+                              setCharaListItem(updateChara);
+                              onClose();
+                            }}
+                            className="cursor-pointer relative w-auto h-[340px]"
+                          >
+                            <WorldBookItem worldBookItem={worldBookItem.worldBook} />
+                          </div>
+                        ))}
+                      </div>
+                    </>
+                  ): (
+                    <>
+                      {!isLogin && (
+                        <div className="w-full h-full flex justify-center items-center  md:col-span-4 sm:col-span-4 col-span-1">
+                          {t("WorldBook.worldbooknull")}
+                        </div>
+                      )}
+                    </> 
                   )}
-                </div>
+
+                  {(firstInit || isLoading) ? (
+                    <div className="absolute left-0 top-0 w-full h-full min-h-[300px] flex justify-center items-center z-10 bg-gray-50/50">
+                      <CircularProgress size="md" aria-label="Loading..."/>
+                    </div>
+                  ) : (
+                    <>
+                      {worldBookPublishList.length === 0 && myWorldBooks.length === 0 && (
+                        <div className="w-full min-h-[400px] h-full flex justify-center items-center  md:col-span-4 sm:col-span-4 col-span-1">
+                          {t("WorldBook.worldbooknull")}
+                        </div>
+                      )}
+                    </>
+                  ) }
+
+                  {(worldBookPublishList.length !== 0) && (
+                    <>
+                      <div className="text-black text-3xl font-semibold">{t("WorldBook.published")}</div>
+                      <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-10 py-10 px-7 overflow-visible h-auto min-h-[400px]">
+                        {worldBookPublishList && worldBookPublishList.map((worldBookItem, index) => (
+                          <div
+                            key={`${worldBookItem.world.uid}+${index}`}
+                            onClick={() => {
+                              const {updateChara} = usePostCharaFun(charaListItem.chara, worldBookItem.world.data);
+                              setCharaListItem(updateChara);
+                              onClose();
+                            }}
+                            className="cursor-pointer relative w-auto h-[340px]"
+                          >
+                            <WorldBookItem worldBookItem={worldBookItem.world.data} />
+                          </div>
+                        ))}
+                        {!worldBookPublishList || worldBookPublishList.length === 0 && (
+                          <div className="w-full h-full min-h-[400px] flex justify-center items-center  md:col-span-4 sm:col-span-4 col-span-1">
+                            {t("WorldBook.worldbooknull")}
+                          </div>
+                        )}
+                      </div>
+                    </>
+                  )}
+                </>
+                
               </ModalBody>
             </>
           )}
