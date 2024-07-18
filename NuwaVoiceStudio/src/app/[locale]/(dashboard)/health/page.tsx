1ec91ae11477b4c286209a9df2ebebd457381c06
zhangke <10066400+zhang-ke001@user.noreply.gitee.com>
2024-07-18 23:09:08 +0800

    feat:add health page

---
+++ NuwaVoiceStudio/src/app/[locale]/(dashboard)/health/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+function SettingsPage() {
+  return (
+    <div>health</div>
+  )
+}
+
+export default SettingsPage
